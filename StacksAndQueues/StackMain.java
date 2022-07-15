31402990182d5c85bbc9eb27ba026430d32dc507
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-15 11:42:31 +0530

    SRART: Stacks and Queues Program

---
+++ StacksAndQueues/StackMain.java
@@ -0,0 +1,8 @@
+package BridgeLabz.StacksAndQueues;
+
+public class StackMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Stacks and Queues program");
+    }
+}
