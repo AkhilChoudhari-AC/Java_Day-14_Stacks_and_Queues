aeaad2312f035003ec0dc89c9aa6dd46079bd766
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-15 11:51:54 +0530

    ADDED: Ability to creat Stack

---
+++ StacksAndQueues/MyNode.java
@@ -0,0 +1,14 @@
+package BridgeLabz.StacksAndQueues;
+
+public class MyNode {
+    public int data;
+    public MyNode next;
+
+    /*
+     * Created Constructor having the same name as class with no return type
+     */
+    public MyNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
--- StacksAndQueues/StackMain.java
+++ StacksAndQueues/StackMain.java
@@ -3,6 +3,17 @@ package BridgeLabz.StacksAndQueues;
 public class StackMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Stacks and Queues program");
+
+        System.out.println("inserting element into stack ::");
+        Stack stack = new Stack();
+
+        /*
+         * 2.Calling addnode method to push value to stack
+         */
+
+        stack.addNode(70);
+        stack.addNode(30);
+        stack.addNode(56);
+
     }
 }
